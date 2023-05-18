7df5f42e3f046dde3a1bb089bc740a3135b9f50f
Harsh Tripathi <103751175+TripathiHARSHji@users.noreply.github.com>
2023-05-19 02:44:51 +0530

    Add files via upload

---
+++ gateway/src/main/java/com/apigateway/mf/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.apigateway.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("Application Gateway Started");
+	}
+
+}
