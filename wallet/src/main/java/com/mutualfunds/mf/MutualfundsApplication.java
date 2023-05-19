f55e57c6aa5d31faa343760fe1c13c4fe3b744c3
Harsh Tripathi <103751175+TripathiHARSHji@users.noreply.github.com>
2023-05-19 06:54:05 +0530

    Update MutualfundsApplication.java

--- wallet/src/main/java/com/mutualfunds/mf/MutualfundsApplication.java
+++ wallet/src/main/java/com/mutualfunds/mf/MutualfundsApplication.java
@@ -17,19 +17,7 @@ public class MutualfundsApplication {
 
 	}
 
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") 
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+
 	}
 
 }
