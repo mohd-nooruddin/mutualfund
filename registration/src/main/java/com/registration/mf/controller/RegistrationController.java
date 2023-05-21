7e948090d50471f4e94c2e5c4f68133e1b784aef
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-05-22 01:36:30 +0530

    Update RegistrationController.java

--- registration/src/main/java/com/registration/mf/controller/RegistrationController.java
+++ registration/src/main/java/com/registration/mf/controller/RegistrationController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.registration.mf.service.RegistrationService;
 
 @RestController
 @RequestMapping("/registration")
+@CrossOrigin
 public class RegistrationController {
 
     private final RegistrationService registrationService;
