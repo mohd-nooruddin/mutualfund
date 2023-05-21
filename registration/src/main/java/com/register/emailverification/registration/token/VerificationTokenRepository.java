b6359796fdd3d0269eddeffc5b2dc7e8e54da27c
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-05-22 01:32:08 +0530

    Delete registration directory

--- registration/src/main/java/com/register/emailverification/registration/token/VerificationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.register.emailverification.registration.token;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByToken(String token);
-}
