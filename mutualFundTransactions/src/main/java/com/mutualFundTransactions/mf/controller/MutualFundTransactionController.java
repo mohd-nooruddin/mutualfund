4f0ce0b1bcca8a674931bf811410dd6699c3015c
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-05-24 06:15:48 +0530

    Add files via upload

---
+++ mutualFundTransactions/src/main/java/com/mutualFundTransactions/mf/controller/MutualFundTransactionController.java
@@ -0,0 +1,34 @@
+package com.mutualFundTransactions.mf.controller;
+
+import com.mutualFundTransactions.mf.entity.MutualFundTransactionEntity;
+import com.mutualFundTransactions.mf.service.MutualFundTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("transactions")
+@CrossOrigin
+public class MutualFundTransactionController {
+
+	private final MutualFundTransactionService transactionService;
+
+	@Autowired
+	public MutualFundTransactionController(MutualFundTransactionService transactionService) {
+		this.transactionService = transactionService;
+	}
+
+	@GetMapping("/user/{userId}")
+	public List<MutualFundTransactionEntity> getTransactionsByUserId(@PathVariable Long userId) {
+		return transactionService.getTransactionsByUserId(userId);
+	}
+
+	@PostMapping("/addTransaction")
+	public void addMutualFundTransaction(@RequestParam("userId") Long userId,
+			@RequestParam("mutualFundsId") Long mutualFundsId, @RequestParam("price") Double price,
+			@RequestParam("unit") Integer unit) {
+		transactionService.addMutualFundTransaction(userId, mutualFundsId, price, unit);
+	}
+}
---
+++ mutualFundTransactions/src/main/java/com/mutualFundTransactions/mf/controller/MutualFundTransactionHistoryController.java
@@ -0,0 +1,39 @@
+package com.mutualFundTransactions.mf.controller;
+
+import com.mutualFundTransactions.mf.entity.MutualFundTransactionHistoryEntity;
+import com.mutualFundTransactions.mf.service.MutualFundTransactionHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("transactionhistory")
+@CrossOrigin
+public class MutualFundTransactionHistoryController {
+
+	private final MutualFundTransactionHistoryService transactionHistoryService;
+
+	@Autowired
+	public MutualFundTransactionHistoryController(MutualFundTransactionHistoryService transactionHistoryService) {
+		this.transactionHistoryService = transactionHistoryService;
+	}
+
+	@PostMapping("/insert")
+	public void insertTransaction(@RequestParam("userId") Long userId, @RequestParam("mutualFundsId") Long mutualFundsId,
+			@RequestParam("type") String type, @RequestParam("price") Double price, @RequestParam("unit") Integer unit, @RequestParam("transactionWallet") Double transactionWallet) {
+		transactionHistoryService.insertTransaction(userId, mutualFundsId, type, price, unit,
+				transactionWallet);
+	}
+
+	@GetMapping("/user/{userId}")
+	public List<MutualFundTransactionHistoryEntity> getTransactionsByUserId(@PathVariable Long userId) {
+		return transactionHistoryService.getTransactionsByUserId(userId);
+	}
+
+	@GetMapping("/type/{type}")
+	public List<MutualFundTransactionHistoryEntity> getTransactionsByType(@PathVariable String type) {
+		return transactionHistoryService.getTransactionsByType(type);
+	}
+}
