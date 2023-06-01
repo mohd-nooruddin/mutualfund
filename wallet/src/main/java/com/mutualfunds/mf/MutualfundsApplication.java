89c7cf2219b7f77c8794e5aaa66afaf22ca308f2
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-06-01 18:30:57 +0530

    Add files via upload

---
+++ wallet/src/main/java/com/mutualfunds/mf/MutualfundsApplication.java
@@ -0,0 +1,20 @@
+package com.mutualfunds.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MutualfundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundsApplication.class, args);
+		System.out.println("MutualFunds Wallet Started");
+
+	}
+
+}
