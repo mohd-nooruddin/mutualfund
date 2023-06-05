dc7f65b99ee6dcd8ad2818bc2ea842ca4148c174
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-06-05 12:54:12 +0530

    Add files via upload

---
+++ mutualFundTransactions/src/main/java/com/mutualFundTransactions/mf/MutualFundTransactionsApplication.java
@@ -0,0 +1,14 @@
+package com.mutualFundTransactions.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualFundTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualFundTransactionsApplication.class, args);
+		
+	}
+
+}
---
+++ mutualFundTransactions/src/main/java/com/mutualFundTransactions/mf/repository/MutualFundTransactionRepository.java
@@ -0,0 +1,36 @@
+package com.mutualFundTransactions.mf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.mutualFundTransactions.mf.entity.MutualFundTransactionEntity;
+
+import java.util.List;
+
+@Repository
+public interface MutualFundTransactionRepository extends JpaRepository<MutualFundTransactionEntity, Long> {
+
+	@Query(value = "SELECT * FROM mutual_fund_transaction WHERE user_id = ?1", nativeQuery = true)
+	List<MutualFundTransactionEntity> findByUserId(Long userId);
+
+	@Query(value = "SELECT COUNT(*) FROM mutual_fund_transaction WHERE user_id = ?2 AND mutual_funds_id = ?1", nativeQuery = true)
+	int findByMutualFundsId(Long mutualFundsId, long userId);
+
+	@Modifying
+	@Query(value = "INSERT INTO mutual_fund_transaction (user_id, mutual_funds_id, price, unit) "
+			+ "VALUES (?1, ?2, ?3, ?4)", nativeQuery = true)
+	void addMutualFundTransaction(Long userId, Long mutualFundsId, Double price, Double unit);
+
+	@Modifying
+	@Query(value = "UPDATE mutual_fund_transaction SET unit = unit + ?1 WHERE user_id = ?2 AND mutual_funds_id = ?3", nativeQuery = true)
+	void updateMutualFundTransaction(Double unit, Long userId, Long mutualFundId);
+
+	@Query(value = "SELECT id FROM user WHERE username = ?", nativeQuery = true)
+	Long getById(String username);
+
+	@Query(value = "SELECT unit FROM mutual_fund_transaction WHERE user_id = ?2 AND mutual_funds_id = ?1", nativeQuery = true)
+	double check(Long mutualFundsId, Long userId);
+	
+}
