aa34a6db709b9e701de1b229f6cd4d87f76466d3
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-06-06 14:39:18 +0530

    Update WishlistController.java

--- wishlist/src/main/java/com/wishlist/mf/controller/WishlistController.java
+++ wishlist/src/main/java/com/wishlist/mf/controller/WishlistController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/wishlist")
 public class WishlistController {
     private final WishlistService wishlistService;
